aae0fbfae9cb04cc450540a926010fb67f7cc66c
Donovan Allen <dallen.928@gmail.com>
2016-12-03 11:42:07 -0500

    hides api key

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var express = require('express')
 var parser = require('body-parser')
 var hbs = require('express-handlebars')
 var mongoose = require('./db/connection')
-
+var http = require('http')
 var app = express();
-
+var restfulAPI = require("./eventful.js");
 var UserEvent = mongoose.model("UserEvent");
 
 app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs")
+
 app.engine(".hbs", hbs({
   extname: ".hbs",
   partialsDir: "view/",
@@ -17,21 +18,18 @@ app.engine(".hbs", hbs({
 }));
 
 app.use("/assets", express.static("public"));
-app.use(parser.urlencoded({extended: true}));
-
+app.use(parser.json({extended: true}));
 
 var randomize = function(array) {
   return array[Math.floor(Math.random() * array.length)]
 }
 
 app.get("/", function(req, res){
-  UserEvent.find({}).then(function(UserEvents){
-      UserEvents = randomize(UserEvents)
-      res.render("index", {
-      UserEvents
-    });
-  })
-});
+  restfulAPI.options
+  restfulAPI.request
+  res.render("index")
+})
+
 
 app.listen(app.get("port"), function () {
   console.log("THIS WORKS OMG");
