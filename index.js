d8dc6b95fe3d7e902b2049fda5e8b385718da9b0
Donovan Allen <dallen.928@gmail.com>
2016-12-19 09:43:56 -0500

    blah

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
 });
 
 app.get("/api/events", function(req, res) {
-  console.log(process.env.eventful_key);
+  console.log(process.env);
 
   UserEvent.count().exec(function(err, count){
 
